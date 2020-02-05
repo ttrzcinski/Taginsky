6c474413b2593f3b3571f1f327d4bf4149fdf2ae
ttrzcinski <trzcinski.tomasz.1988@gmail.com>
2020-02-06 00:13:07 +0100

    Added VersionUtilTest.

---
+++ test/org/ttrzcinski/utils/VersionUtilTest.java
@@ -0,0 +1,98 @@
+package org.ttrzcinski.utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VersionUtilTest {
+
+    @Test
+    void asVersion_nulls() {
+        // Arrange
+        Integer test_major = null;
+        Integer test_minor = null;
+        Integer test_build = null;
+        String expected = "0.0.0";
+
+        // Act
+        String actual = VersionUtil.asVersion(test_major, test_minor, test_build);
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void asVersion_negatives() {
+        // Arrange
+        Integer test_major = -1;
+        Integer test_minor = -2;
+        Integer test_build = -3;
+        String expected = "1.2.3";
+
+        // Act
+        String actual = VersionUtil.asVersion(test_major, test_minor, test_build);
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void asVersion_positives() {
+        // Arrange
+        Integer test_major = 1;
+        Integer test_minor = 2;
+        Integer test_build = 3;
+        String expected = "1.2.3";
+
+        // Act
+        String actual = VersionUtil.asVersion(test_major, test_minor, test_build);
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void isAnySet_withAllSet() {
+        // Arrange
+        Integer test_major = 1;
+        Integer test_minor = 2;
+        Integer test_build = 3;
+        boolean expected = true;
+
+        // Act
+        boolean actual = VersionUtil.isAnySet(test_major, test_minor, test_build);
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void isAnySet_withMixed() {
+        // Arrange
+        Integer test_major = null;
+        Integer test_minor = 2;
+        Integer test_build = null;
+        boolean expected = true;
+
+        // Act
+        boolean actual = VersionUtil.isAnySet(test_major, test_minor, test_build);
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void isAnySet_withAllUnset() {
+        // Arrange
+        Integer test_major = null;
+        Integer test_minor = null;
+        Integer test_build = null;
+        boolean expected = false;
+
+        // Act
+        boolean actual = VersionUtil.isAnySet(test_major, test_minor, test_build);
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+}
