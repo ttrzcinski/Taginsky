cb1ca1149621f1130efdf1a94a7a7c5f23347a06
Tomasz_Trzcinski <Tomasz_Trzcinski>
2020-03-11 00:47:40 +0100

    Moved to better location.

--- test/java/org/ttrzcinski/utils/VersionUtilTest.java
+++ test/java/org/ttrzcinski/utils/VersionUtilTest.java
@@ -2,7 +2,7 @@ package org.ttrzcinski.utils;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class VersionUtilTest {
 
